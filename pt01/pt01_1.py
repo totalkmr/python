95a66142bdb077c3fa496dc4000e238a3ab6ecec
totalkmr <totalkmr@mail.ru>
2020-04-08 13:55:06 +0700

    Add pt01

---
+++ pt01/pt01_1.py
@@ -0,0 +1,17 @@
+a = 30
+b = 6.6
+c = 'Hello'
+d = ' world!'
+
+print(a + b)
+print(c + d)
+
+name = input('Как ваше имя? ')
+age = input('Ваш возраст? ')
+experience = input('Ваш опыт работы по специальности (полных лет)? ')
+mood = input('Как настроение? ')
+
+print('Имя:', name)
+print('Возраст:', age)
+print('Опыт работы:', experience)
+print('Настроение:', mood)
